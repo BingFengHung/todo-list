37859f00fddb31816cf8d953cc27a77d25d8ded2
BingFengHung <joe94008@gmail.com>
2024-06-11 09:48:19 +0800

    <feat>: 建立基本的 todolist 邏輯

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const todos = [
+  {
+    "id": 1,
+    "todo": "Walking Dog",
+    "isComplete": false
+  },
+  {
+    "id": 2,
+    "todo": "Do the laundary",
+    "isComplete": false
+  }
+]
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [items, setItems] = useState(todos);
+  const [isPopup , setIsPopup] = useState(false);
+  const [text, setText] = useState('');
+  
+  const popupShow = () => {
+    
+  }
+  
+  const addItems = () => {
+    todos.push({
+      id: items.length + 1,
+      todo: text,
+      isComplete: false
+    })
+    
+    setItems([...todos])
+    setText('')
+    setIsPopup(false)
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setIsPopup(!isPopup)}>New Task</button>
+      <ul>
+        {items.map(item => {
+          return (
+            <li key={item.id}>{item.todo}</li>
+          )
+        })
+        }
+      </ul>
+      { 
+        isPopup && 
+        <div>
+        <input value={text} onChange={(el) => setText(el.target.value)}/>
+        <button onClick={addItems}>Add</button>
+        <button onClick={() => setText('')}>Reset</button>
+        </div>
+      }
     </>
   )
 }
